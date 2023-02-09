414f0f1792169a7c672086e77fc10d5a5bfb40d8
Anandbhovimech <124783482+Anandbhovimech@users.noreply.github.com>
2023-02-09 17:21:06 +0530

    Add files via upload

---
+++ anand/2.py
@@ -0,0 +1,26 @@
+x=10
+y = x+2
+print(id(y))
+ y=x+5
+ print(id(y)) 
+ print y  
+ 
+ 
+x=10
+print(x)
+x="hello world")
+print(x)
+
+
+ans=10/0
+print(ans)
+    sqr(number):
+    res=num*num
+    print(res)    
+inp=int input("enter the number")
+sqr(inp)
+
+
+
+print
+    
---
+++ anand/a1.py
@@ -0,0 +1,8 @@
+def sqr(num):
+    result = number * number
+    print("square root=",res)
+    
+    
+    
+    
+sqr(2)
